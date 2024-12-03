e259403f55c636d37e329f05dcf9ab4990298968
Nithwin <vmnithwin@gmail.com>
2024-12-03 23:06:50 +0530

    error fixed

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://nithwin-blog.vercel.app/', // Replace with the frontend URL if different
+  origin: 'https://nithwin-blog.vercel.app', // Replace with the frontend URL if different
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
